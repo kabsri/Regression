e036d80abbc0e485ac67b370412efe110daf9afb
Kabilan Sriranjan <kabilan.s97@gmail.com>
2017-11-08 15:48:51 -0500

    started assignment 3

--- Assignment-3/ass3.R
+++ Assignment-3/ass3.R
@@ -1,1 +1,11 @@
 data = read.csv("http://www.math.mcgill.ca/yyang/regression/data/cigs.csv" , header=TRUE)
+y = data$CO
+x1 = data$TAR
+x2 = data$NICOTINE
+x3 = data$WEIGHT
+
+X = matrix(c(x1, x2, x3), length(x1), 3)
+fit.Cigs = lm(y~X)
+summary(fit.Cigs)
+
+plot(fit.Cigs)
